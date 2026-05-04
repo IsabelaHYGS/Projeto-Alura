f2db1f69a703bfe44b434a3e2d320e18480cd197
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2026-05-04 13:12:34 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -14,8 +14,11 @@ for (let i = 0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
@@ -28,6 +31,22 @@ let dias = Math.floor(horas / 24);
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
-    
+
+if (tempoFinal > 0){
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+  return "Prazo Finalizado";
 }
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
